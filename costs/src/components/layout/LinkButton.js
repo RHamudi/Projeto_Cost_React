c01f817267f61316a1ab7060189374908c669b9a
RHamudi <94305379+RHamudi@users.noreply.github.com>
2022-04-14 12:57:21 -0300

    Finalizando a pagina HOME

---
+++ costs/src/components/layout/LinkButton.js
@@ -0,0 +1,11 @@
+import styles from './LinkButton.module.css'
+import {Link} from 'react-router-dom'
+
+function LinkButton({to, text}) {
+    return (
+        <Link>
+        </Link>
+    )
+}
+
+export default LinkButton
